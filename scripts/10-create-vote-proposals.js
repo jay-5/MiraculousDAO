7ce9595ee7e144d5f9cbeb1ff15454e97e26709e
Jay Jayewardene <raj69@case.edu>
2023-01-04 02:29:08 -0500

    new proposals

--- scripts/10-create-vote-proposals.js
+++ scripts/10-create-vote-proposals.js
@@ -2,74 +2,23 @@ import sdk from "./1-initialize-sdk.js";
 import { ethers } from "ethers";
 
 (async () => {
-  try {
-    // This is our governance contract.
-    const vote = await sdk.getContract("0x5883De5502B98aDDB651eF0E95AA8bD9Da813cfC", "vote");
-    // This is our ERC-20 contract.
-    const token = await sdk.getContract("0xB2ad1Aa9Ff5c27858744F2B9A2a5ad5C67bf9a80", "token");
-    // Create proposal to mint 40 new token to the treasury.
-    const amount = 40;
-    const description = "Should the DAO mint an additional " + amount + " tokens into the treasury?";
-    const executions = [
-      {
-        // Our token contract that actually executes the mint.
-        toAddress: token.getAddress(),
-        // Our nativeToken is ETH. nativeTokenValue is the amount of ETH we want
-        // to send in this proposal. In this case, we're sending 0 ETH.
-        // We're just minting new tokens to the treasury. So, set to 0.
-        nativeTokenValue: 0,
-        // We're doing a mint! And, we're minting to the vote, which is
-        // acting as our treasury.
-        // in this case, we need to use ethers.js to convert the amount
-        // to the correct format. This is because the amount it requires is in wei.
-        transactionData: token.encoder.encode(
-          "mintTo", [
-          vote.getAddress(),
-          ethers.utils.parseUnits(amount.toString(), 18),
-        ]
-        ),
-      }
-    ];
 
-    await vote.propose(description, executions);
-
-    console.log("✅ Successfully created proposal to mint tokens");
-  } catch (error) {
-    console.error("failed to create first proposal", error);
-    process.exit(1);
-  }
-
+  
   try {
     // This is our governance contract.
     const vote = await sdk.getContract("0x5883De5502B98aDDB651eF0E95AA8bD9Da813cfC", "vote");
     // This is our ERC-20 contract.
     const token = await sdk.getContract("0xB2ad1Aa9Ff5c27858744F2B9A2a5ad5C67bf9a80", "token");
     // Create proposal to transfer ourselves 5 tokens for being awesome.
-    const amount = 5;
-    const description = "Should the DAO transfer " + amount + " tokens from the treasury to " +
-      process.env.WALLET_ADDRESS + " for being awesome?";
-    const executions = [
-      {
-        // Again, we're sending ourselves 0 ETH. Just sending our own token.
-        nativeTokenValue: 0,
-        transactionData: token.encoder.encode(
-          // We're doing a transfer from the treasury to our wallet.
-          "transfer",
-          [
-            process.env.WALLET_ADDRESS,
-            ethers.utils.parseUnits(amount.toString(), 18),
-          ]
-        ),
-        toAddress: token.getAddress(),
-      },
-    ];
-
+    const newTool = "New Dev Tool";
+    const description = "Should add " + newTool + " to the Miraculous Marketplace? ";
+    const executions = console.log(newTool + " successfully added to the marketplace");
     await vote.propose(description, executions);
 
     console.log(
-      "✅ Successfully created proposal to reward ourselves from the treasury, let's hope people vote for it!"
+      "✅ Successfully created proposal to add New Dev Tool to the Marketplace, let's hope people vote for it!"
     );
   } catch (error) {
-    console.error("failed to create second proposal", error);
+    console.error("failed to create additional proposal", error);
   }
 })();
