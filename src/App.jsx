45dd7906e09b8a2dcb1d4b40f709695f0974e799
Jay Jayewardene <raj69@case.edu>
2023-01-04 01:50:43 -0500

    create proposal

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useWeb3Context } from '@web3-react/core';
 import {
   useAddress,
   useNetwork,
@@ -9,6 +10,8 @@ import {
 import { ChainId } from '@thirdweb-dev/sdk';
 import { useState, useEffect, useMemo } from 'react';
 import { AddressZero } from '@ethersproject/constants';
+
+
 
 const App = () => {
   // Use the hooks thirdweb give us.
@@ -30,6 +33,26 @@ const App = () => {
     '0x5883De5502B98aDDB651eF0E95AA8bD9Da813cfC',
     'vote',
   );
+
+  const createProposal = async () => {
+    try {
+      // Send a transaction to the contract to create a proposal
+      await vote.create(
+        // The proposal description
+        'Your proposal description',
+        // The number of tokens needed to vote on this proposal
+        '100',
+        // The address of the token contract
+        token.address,
+        // The duration of the voting period in seconds
+        '86400'
+      );
+      console.log('Proposal created successfully!');
+    } catch (error) {
+      console.error('Error creating proposal:', error);
+    }
+  };
+
   // Hook to check if the user has our NFT
   const { data: nftBalance } = useNFTBalance(editionDrop, address, '0');
 
@@ -69,19 +92,19 @@ const App = () => {
     };
     getAllProposals();
   }, [hasClaimedNFT, vote]);
-
+  
   // We also need to check if the user already voted.
   useEffect(() => {
     if (!hasClaimedNFT) {
       return;
     }
-
+  
     // If we haven't finished retrieving the proposals from the useEffect above
     // then we can't check if the user voted yet!
     if (!proposals.length) {
       return;
     }
-
+  
     const checkIfUserHasVoted = async () => {
       try {
         const hasVoted = await vote.hasVoted(proposals[0].proposalId, address);
@@ -103,264 +126,101 @@ const App = () => {
     if (!hasClaimedNFT) {
       return;
     }
-
-    // Just like we did in the 7-airdrop-token.js file! Grab the users who hold our NFT
-    // with tokenId 0.
-    const getAllAddresses = async () => {
+  
+    const getAllMembers = async () => {
       try {
-        const memberAddresses =
-          await editionDrop?.history.getAllClaimerAddresses(0);
-        setMemberAddresses(memberAddresses);
-        console.log('🚀 Members addresses', memberAddresses);
+        const members = await editionDrop.getAllMembers();
+        setMemberAddresses(members);
+        console.log('Members:', members);
+        const memberTokenAmounts = [];
+        for (const member of members) {
+          const tokenAmount = await token.balanceOf(member);
+          console.log('Token amount:', tokenAmount);
+          memberTokenAmounts.push(tokenAmount);
+        }
+        setMemberTokenAmounts(memberTokenAmounts);
       } catch (error) {
-        console.error('failed to get member list', error);
+        console.error('Error getting member addresses:', error);
       }
     };
-    getAllAddresses();
-  }, [hasClaimedNFT, editionDrop?.history]);
-
-  // This useEffect grabs the # of token each member holds.
-  useEffect(() => {
-    if (!hasClaimedNFT) {
-      return;
-    }
-
-    const getAllBalances = async () => {
-      try {
-        const amounts = await token?.history.getAllHolderBalances();
-        setMemberTokenAmounts(amounts);
-        console.log('👜 Amounts', amounts);
-      } catch (error) {
-        console.error('failed to get member balances', error);
-      }
-    };
-    getAllBalances();
-  }, [hasClaimedNFT, token?.history]);
-
-  // Now, we combine the memberAddresses and memberTokenAmounts into a single array
-  const memberList = useMemo(() => {
-    return memberAddresses.map((address) => {
-      // We're checking if we are finding the address in the memberTokenAmounts array.
-      // If we are, we'll return the amount of token the user has.
-      // Otherwise, return 0.
-      const member = memberTokenAmounts?.find(
-        ({ holder }) => holder === address,
-      );
-
-      return {
-        address,
-        tokenAmount: member?.balance.displayValue || '0',
-      };
-    });
-  }, [memberAddresses, memberTokenAmounts]);
-
-  if (address && network?.[0].data.chain.id !== ChainId.Goerli) {
-    return (
-      <div className="unsupported-network">
-        <h2>Please connect to Goerli</h2>
-        <p>
-          This dapp only works on the Goerli network, please switch networks in
-          your connected wallet.
-        </p>
-      </div>
-    );
-  }
-
-  // This is the case where the user hasn't connected their wallet
-  // to your web app. Let them call connectWallet.
-  if (!address) {
-    return (
-      <div className="landing">
-        <h1>Welcome to MiraculousDAO ✨</h1>
-        <div className="btn-hero">
-          <ConnectWallet />
-        </div>
-      </div>
-    );
-  }
-
-  // If the user has already claimed their NFT we want to display the interal DAO page to them
-  // only DAO members will see this. Render all the members + token amounts.
-  if (hasClaimedNFT) {
-    return (
-      <div className="member-page">
-        <h1>MiraculousDAO ✨ Member Page</h1>
-        <p>Congratulations on being a member</p>
-        <div>
-          <div>
-            <h2>Member List</h2>
-            <table className="card">
-              <thead>
-                <tr>
-                  <th>Address</th>
-                  <th>Token Amount</th>
-                </tr>
-              </thead>
-              <tbody>
-                {memberList.map((member) => {
-                  return (
-                    <tr key={member.address}>
-                      <td>{shortenAddress(member.address)}</td>
-                      <td>{member.tokenAmount}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-          <div>
-            <h2>Active Proposals</h2>
-            <form
-              onSubmit={async (e) => {
-                e.preventDefault();
-                e.stopPropagation();
-
-                //before we do async things, we want to disable the button to prevent double clicks
-                setIsVoting(true);
-
-                // lets get the votes from the form for the values
-                const votes = proposals.map((proposal) => {
-                  const voteResult = {
-                    proposalId: proposal.proposalId,
-                    //abstain by default
-                    vote: 2,
-                  };
-                  proposal.votes.forEach((vote) => {
-                    const elem = document.getElementById(
-                      proposal.proposalId + '-' + vote.type,
-                    );
-
-                    if (elem.checked) {
-                      voteResult.vote = vote.type;
-                      return;
-                    }
-                  });
-                  return voteResult;
-                });
-
-                // first we need to make sure the user delegates their token to vote
-                try {
-                  //we'll check if the wallet still needs to delegate their tokens before they can vote
-                  const delegation = await token.getDelegationOf(address);
-                  // if the delegation is the 0x0 address that means they have not delegated their governance tokens yet
-                  if (delegation === AddressZero) {
-                    //if they haven't delegated their tokens yet, we'll have them delegate them before voting
-                    await token.delegateTo(address);
-                  }
-                  // then we need to vote on the proposals
-                  try {
-                    await Promise.all(
-                      votes.map(async ({ proposalId, vote: _vote }) => {
-                        // before voting we first need to check whether the proposal is open for voting
-                        // we first need to get the latest state of the proposal
-                        const proposal = await vote.get(proposalId);
-                        // then we check if the proposal is open for voting (state === 1 means it is open)
-                        if (proposal.state === 1) {
-                          // if it is open for voting, we'll vote on it
-                          return vote.vote(proposalId, _vote);
-                        }
-                        // if the proposal is not open for voting we just return nothing, letting us continue
-                        return;
-                      }),
-                    );
-                    try {
-                      // if any of the propsals are ready to be executed we'll need to execute them
-                      // a proposal is ready to be executed if it is in state 4
-                      await Promise.all(
-                        votes.map(async ({ proposalId }) => {
-                          // we'll first get the latest state of the proposal again, since we may have just voted before
-                          const proposal = await vote.get(proposalId);
-
-                          //if the state is in state 4 (meaning that it is ready to be executed), we'll execute the proposal
-                          if (proposal.state === 4) {
-                            return vote.execute(proposalId);
-                          }
-                        }),
-                      );
-                      // if we get here that means we successfully voted, so let's set the "hasVoted" state to true
-                      setHasVoted(true);
-                      // and log out a success message
-                      console.log('successfully voted');
-                    } catch (err) {
-                      console.error('failed to execute votes', err);
-                    }
-                  } catch (err) {
-                    console.error('failed to vote', err);
-                  }
-                } catch (err) {
-                  console.error('failed to delegate tokens');
-                } finally {
-                  // in *either* case we need to set the isVoting state to false to enable the button again
-                  setIsVoting(false);
-                }
-              }}
-            >
-              {proposals.map((proposal) => (
-                <div key={proposal.proposalId} className="card">
-                  <h5>{proposal.description}</h5>
-                  <div>
-                    {proposal.votes.map(({ type, label }) => (
-                      <div key={type}>
-                        <input
-                          type="radio"
-                          id={proposal.proposalId + '-' + type}
-                          name={proposal.proposalId}
-                          value={type}
-                          //default the "abstain" vote to checked
-                          defaultChecked={type === 2}
-                        />
-                        <label htmlFor={proposal.proposalId + '-' + type}>
-                          {label}
-                        </label>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              ))}
-              <button disabled={isVoting || hasVoted} type="submit">
-                {isVoting
-                  ? 'Voting...'
-                  : hasVoted
-                  ? 'You Already Voted'
-                  : 'Submit Votes'}
-              </button>
-              {!hasVoted && (
-                <small>
-                  This will trigger multiple transactions that you will need to
-                  sign.
-                </small>
-              )}
-            </form>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  // Render mint nft screen.
+    getAllMembers();
+  }, [hasClaimedNFT, editionDrop, token]);
+  
   return (
-    <div className="mint-nft">
-      <h1>Mint your free MiraculousDAO ✨ Membership NFT</h1>
-      <div className="btn-hero">
-        <Web3Button
-          contractAddress={editionDropAddress}
-          action={(contract) => {
-            contract.erc1155.claim(0, 1);
+    <div>
+      {/* Connect to a wallet using the thirdweb components */}
+      <ConnectWallet
+        title="Connect Wallet"
+        titleConnecting="Connecting to Wallet"
+        titleConnected="Connected to Wallet"
+        description="Connect your Ethereum wallet to start voting."
+        providerOptions={{
+          walletconnect: {
+            rpcUrl: 'https://mainnet.eth.aragon.network/',
+          },
           }}
-          onSuccess={() => {
-            console.log(
-              `🌊 Successfully Minted! Check it out on OpenSea: https://testnets.opensea.io/assets/${editionDrop.getAddress()}/0`,
-            );
-          }}
-          onError={(error) => {
-            console.error('Failed to mint NFT', error);
-          }}
-        >
-          Mint your NFT (FREE)
-        </Web3Button>
-      </div>
-    </div>
-  );
-};
-
-export default App;
+            >
+              {/* Show a button to connect to a wallet */}
+              <Web3Button />
+            </ConnectWallet>
+            {/* If the user is connected to a wallet, show them their address and the network they are on. */}
+            {address && (
+              <div>
+                <h3>Your Wallet</h3>
+                <p>
+                  {shortenAddress(address)} on {network.name}
+                </p>
+                {/* If the user has our NFT, show them the voting options */}
+                {hasClaimedNFT && (
+                  <div>
+                    <h3>Voting</h3>
+                    {/* If there are no proposals, show a button to create one */}
+                    {!proposals.length && (
+                      <div>
+                        <button onClick={createProposal}>Create Proposal</button>
+                      </div>
+                    )}
+                    {/* If there are proposals, show a form to vote on them */}
+                    {proposals.length > 0 && (
+                      <
+                      form>
+                      <h3>Proposals</h3>
+                      {proposals.map((proposal, i) => (
+                        <div key={i}>
+                          <p>{proposal.name}</p>
+                          <button
+                            onClick={() => {
+                              setIsVoting(true);
+                              vote.vote(proposal.proposalId, true, {
+                                gasLimit: 300000,
+                              });
+                            }}
+                            disabled={isVoting || hasVoted}
+                          >
+                            Yes
+                          </button>
+                          <button
+                            onClick={() => {
+                              setIsVoting(true);
+                              vote.vote(proposal.proposalId, false, {
+                                gasLimit: 300000,
+                              });
+                            }}
+                            disabled={isVoting || hasVoted}
+                          >
+                            No
+                          </button>
+                        </div>
+                      ))}
+                    </form>
+                  )}
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+        );
+        };
+        
+        export default App;
+        
